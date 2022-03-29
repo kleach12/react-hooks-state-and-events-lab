747e5fec31d5a4f79d70f362af811ea775e75e9e
Kale Leach <kleach1212@gmail.com>
2022-03-29 12:00:05 -0700

    commit tree f55a7391103064db6d22d413215497b12f5d86bc

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState("true")
+
+  function handleCart(){
+    setAddToCart((addToCart) => !addToCart)
+  }
   return (
-    <li className="">
+    <li className= {addToCart ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{addToCart ? "Add to Cart" : "In Cart"}</button>
     </li>
   );
 }
